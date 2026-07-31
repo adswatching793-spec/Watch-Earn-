5881a28c1589150ef9217475dbe14c3f412e05d5
adswatching793-spec <adswatching793@gmail.com>
2026-08-01 00:15:17 +0500

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,1 +1,13 @@
+function showLogin(){
+    document.getElementById("popup").style.display="block";
+    document.getElementById("title").innerHTML="Login";
+}
 
+function showRegister(){
+    document.getElementById("popup").style.display="block";
+    document.getElementById("title").innerHTML="Register Now";
+}
+
+function closeBox(){
+    document.getElementById("popup").style.display="none";
+}
